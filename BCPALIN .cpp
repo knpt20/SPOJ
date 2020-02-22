ea55679754515785f3026c6e26836c8a56b326e4
sean <54012472+knpt20@users.noreply.github.com>
2020-02-22 11:02:45 +0700

    Add files via upload

---
+++ BCPALIN .cpp
@@ -0,0 +1,29 @@
+#pragma warning(disable : 4996)
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool isPalindrome(string s) {
+	for (int i = 0, j = s.length() - 1; i < j; i++, j--)
+		if (s[i] != s[j])
+			return false;
+	return true;
+}
+
+int main()
+{
+	//freopen("input.txt", "r", stdin);
+	
+
+	int t;
+	cin >> t;
+	while (t--) {
+		string s;
+		cin >> s;
+		if (isPalindrome(s))
+			cout << "YES\n";
+		else cout << "NO\n";
+	}
+}
