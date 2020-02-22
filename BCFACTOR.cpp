e2f9115ccfafed496f97e4e52ccf0e2d66c3e8a3
sean <54012472+knpt20@users.noreply.github.com>
2020-02-22 15:17:04 +0700

    Add files via upload

---
+++ BCFACTOR.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	for (int i = 2; i <= sqrt(n); i++) {
+		if (n%i == 0){
+			int cnt = 0;
+			while (n % i == 0) {
+				cnt++;
+				n /= i;
+			}
+			cout << i << " " << cnt << "\n";
+		}
+	}
+
+	if (n > 1)
+		cout << n << " " << 1;
+
+	return 0;
+}
+
